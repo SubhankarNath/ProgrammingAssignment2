9e6ac3aab5977a820d2de6c46aebd36700f3b005
SubhankarNath <subhankarnath61@gmail.com>
2017-12-16 13:58:08 +0530

    create a matrix and find its inverse
    create a matrix and find its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
+##This function creates a special matrix that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setinverse<-function(solve) m<<-solve
+  getinverse<-function() m
+  list(set=set,get=get,
+       setinverse=setinverse,
+       getinverse=getinverse
+  )
 }
 
-
 ## Write a short comment describing this function
+## This function calculates the inverse of the generated matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse()
+  if(!is.null(m)){
+    print("Getting the inverse from the store!")
+    return(m)
+  }
+  inv<-x$get()
+  m<-solve(inv)
+  x$setinverse(m)
+  m
 }
